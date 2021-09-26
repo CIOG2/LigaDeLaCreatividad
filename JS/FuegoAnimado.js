7a92dd41a73e9dac8d956a659fee61a82349107f
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-26 14:14:37 -0600

    Anadi identificadores al HTMLL para comenzar a trabajar en JS

---
+++ JS/FuegoAnimado.js
@@ -0,0 +1,15 @@
+let Espacio = document.getElementById("space_background")
+let Cielo = document.getElementById("cielo_background")
+let Fuego1 = document.getElementById("fuego1")
+let Fuego2 = document.getElementById("fuego2")
+
+
+if (condition) {
+    Fuego1.classList.add("Fuego__animacion");
+    Fuego2.classList.add("Fuego__animacion");        
+}
+
+if (condition) {
+    Fuego1.classList.remove("Fuego__animacion");
+    Fuego2.classList.remove("Fuego__animacion");        
+}
