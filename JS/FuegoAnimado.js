9e210f2d19b3d2f429180ada74de6cb053196e6d
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-26 18:45:54 -0600

    añadi un efecto para que no se vea tan sosa la manera de como se apaga el fuego

--- JS/FuegoAnimado.js
+++ JS/FuegoAnimado.js
@@ -2,7 +2,7 @@ let Espacio = document.getElementById("space_background")
 let Cielo = document.getElementById("cielo_background")
 let Fuego1 = document.getElementById("fuego1")
 let Fuego2 = document.getElementById("fuego2")
-
+let confirmacion = false;
 
 const observerEspacio = new IntersectionObserver(ApagarFuego, {threshold : 0.40});        
 const observerCielo = new IntersectionObserver(EncenderFuego, {threshold : 0.25});        
@@ -13,17 +13,24 @@ observerCielo.observe(Cielo);
 function EncenderFuego(Cielo){
     const cielos = Cielo[0];
     
+
     if (cielos.intersectionRatio >= 0.25) {
+        Fuego1.classList.remove("Fuego__animacion_apagar");
+        Fuego2.classList.remove("Fuego__animacion_apagar");
         Fuego1.classList.add("Fuego__animacion");
-        Fuego2.classList.add("Fuego__animacion");  
+        Fuego2.classList.add("Fuego__animacion");
+        confirmacion = true;
     }
 }
 
 function ApagarFuego(Espacio) {
     const space = Espacio[0]; 
 
-    if (space.intersectionRatio >= 0.40) {
+    if (space.intersectionRatio >= 0.40 && confirmacion == true) {
         Fuego1.classList.remove("Fuego__animacion");
         Fuego2.classList.remove("Fuego__animacion");  
+        Fuego1.classList.add("Fuego__animacion_apagar");
+        Fuego2.classList.add("Fuego__animacion_apagar");  
+        confirmacion = false;
     }    
 }
