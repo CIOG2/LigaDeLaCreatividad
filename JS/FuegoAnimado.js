30d16467bff59955daef6080528ea3314db449be
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-26 18:27:09 -0600

    Agregue la funcion para que cuando el meteoro cruce la atmosfera se prenda en llamas

--- JS/FuegoAnimado.js
+++ JS/FuegoAnimado.js
@@ -4,12 +4,26 @@ let Fuego1 = document.getElementById("fuego1")
 let Fuego2 = document.getElementById("fuego2")
 
 
-if (condition) {
-    Fuego1.classList.add("Fuego__animacion");
-    Fuego2.classList.add("Fuego__animacion");        
+const observerEspacio = new IntersectionObserver(ApagarFuego, {threshold : 0.40});        
+const observerCielo = new IntersectionObserver(EncenderFuego, {threshold : 0.25});        
+observerEspacio.observe(Espacio);
+observerCielo.observe(Cielo);
+
+
+function EncenderFuego(Cielo){
+    const cielos = Cielo[0];
+    
+    if (cielos.intersectionRatio >= 0.25) {
+        Fuego1.classList.add("Fuego__animacion");
+        Fuego2.classList.add("Fuego__animacion");  
+    }
 }
 
-if (condition) {
-    Fuego1.classList.remove("Fuego__animacion");
-    Fuego2.classList.remove("Fuego__animacion");        
+function ApagarFuego(Espacio) {
+    const space = Espacio[0]; 
+
+    if (space.intersectionRatio >= 0.40) {
+        Fuego1.classList.remove("Fuego__animacion");
+        Fuego2.classList.remove("Fuego__animacion");  
+    }    
 }
